38bd0be511ea218ee196588087f539d4f1ebddcd
InaKrapp <59924357+InaKrapp@users.noreply.github.com>
2024-05-20 21:20:26 +0200

    Added new files
    Added new files with major changes: The system now automatically realizes if it is working on a German or English-language computer and adjusts the language accordingly. Also, it supports FasterWhisper now.

---
+++ Wisp.py
@@ -0,0 +1,405 @@
+import sys
+import torch
+import pyaudio
+import wave
+from PyQt6.QtWidgets import (QApplication, QWidget, QLineEdit, QTextEdit, QLabel, QPushButton, QGridLayout, QFileDialog, QInputDialog, QMessageBox)
+from PyQt6.QtCore import QStandardPaths
+from pathlib import Path
+from pydub import AudioSegment
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from faster_whisper import WhisperModel
+import os
+from lang import get_text as tx
+
+class MainWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+
+        # set the window title
+        self.setWindowTitle(tx("Window_title"))
+        self.setGeometry(100, 100, 400, 100)
+
+        # Create a layout
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        # Create a line widget: QLabel will display what is typed into QLineEdit
+        self.button_record = QPushButton(tx('Start_voice_recording'))
+        self.button_record.clicked.connect(self.record_speech)
+        self.file_record = "output"
+        self.recording = True
+        self.button_state = "record"
+        self.recorder = Recorder(channels=2, rate=16000, frames_per_buffer=1024)
+        self.recording_edit = QTextEdit()
+        self.recording_edit.setText(tx("Voice_recording_not_yet_started"))
+        #self.filename_record = QLineEdit()
+
+        # Create a button to allow the user to choose and file and display the name of the chosen file.
+        file_browse = QPushButton(tx('Select_a_file'))
+        file_browse.clicked.connect(self.open_file_dialog)
+        self.filename_edit = QLineEdit()
+
+        # Set defaults
+        self.accuracy = 2
+        self.translation= 0
+
+        # Create a button that allows to choose accuracy.
+        accuracy_button = QPushButton(tx('Select_speed_and_accuracy'))
+        accuracy_button.clicked.connect(self.choose_accuracy)
+
+        # Display default option at the start. Display choosen option if the user changes it.
+        self.accuracy_edit = QLineEdit()
+        self.accuracy_edit.setText(tx("Default_precision"))
+
+
+        # Create a button that allows to choose if the audio should be translated
+        translation_button = QPushButton(tx("Translation_selection"))
+        translation_button.clicked.connect(self.choose_translation)
+        # Display default option at the start. Display choosen option if the user changes it.
+        self.translation_edit = QLineEdit()
+        self.filepath_edit = QLineEdit()
+        self.translation_edit.setText(tx("Default_translation"))
+
+        # Create a button that allows the user to start the transcription process and show at the beginning that transcription has not started yet.
+        self.transcription_edit = QTextEdit()
+        self.transcription_edit.setText(tx("Transcription_not_started_yet"))
+        file_transcribe = QPushButton(tx('Start_transcription'))
+
+        # Display the message that the transcription process started and start the transcription process.
+        file_transcribe.pressed.connect(self.transcription_message)
+        file_transcribe.clicked.connect(self.transcribe)
+
+        # Create a button that allows to close the program
+        close_button = QPushButton(tx("Close_program"), self)
+        close_button.clicked.connect(self.close)
+
+        # Place all created buttons on the layout. 
+        # Place the button and text field for recording speech:
+        layout.addWidget(QLabel(tx("Whisper_words_to_text")))
+        layout.addWidget(QLabel(tx("Record_something")))
+        layout.addWidget(self.button_record, 2, 0)
+        layout.addWidget(self.recording_edit, 3, 0)
+        # Place the file browser button and the field which shows the currently choosen file
+        layout.addWidget(QLabel(tx("Choose_file")), 4, 0)
+        layout.addWidget(file_browse, 5, 0) 
+        layout.addWidget(QLabel(tx("Choosen_file")), 6, 0)
+        layout.addWidget(self.filename_edit, 7,0) 
+
+        # Add the other two buttons to define accuracy and translation
+        layout.addWidget(accuracy_button, 8, 0)
+        layout.addWidget(self.accuracy_edit, 9, 0)
+        layout.addWidget(translation_button, 10, 0)
+        layout.addWidget(self.translation_edit, 11, 0)
+
+        # Add the button to start the transcription process
+        layout.addWidget(file_transcribe, 12, 0)
+        layout.addWidget(self.transcription_edit, 13, 0)
+        layout.addWidget(close_button, 14, 0)
+
+        # show the windows
+        self.show()
+    def record_speech(self):
+        "This method allows the user to record their own speech, starting and stopping a recording."
+        if self.button_state == 'record':
+            # Show that recording has started:
+            self.recording_edit.setText(tx('Voice_recording_started'))
+            # Change filepath to the path where the recording should be stored.
+            filepath = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.DocumentsLocation)
+            if not os.path.exists(filepath + tx("folder_name")):
+                os.mkdir(filepath + tx("folder_name"))
+            filepath = filepath + tx("folder_name")
+            os.chdir(filepath)
+            # Select a name to store the recording under
+            fileword = tx("file_name")
+            filename = fileword+".wav"
+            # Check that file "Sprachaufnahme" does not exist yet. If it does, rename the new file to "Sprachaufnahme_2", "Sprachaufnahme_3" and so on.
+            file = Path(f"{filename}")
+            fileindex = 2
+            while file.exists() == True:
+                filename = f"{fileword}_{fileindex}.wav"
+                fileindex = fileindex + 1
+                file = Path(f"{filename}")
+            # Make filename available to be displayed once the recording has finished.
+            path = Path(filename)
+            self.filename_edit.setText(str(path.name))
+            self.filename = filename
+            self.filename_path = path
+            try: # Open file where to save the recording
+                self.recFile = self.recorder.open(f"{filename}", 'wb')
+                # Start recording
+                self.recFile.start_recording()
+            except OSError:
+                self.recording_edit.setText(tx("Microphone_not_recognized"))
+                return
+            # Change button text to 'Sprachaufnahme beenden'
+            self.button_record.setText(tx("Stop_recording"))
+            self.button_state = 'stop recording'
+        else:
+            # If the user presses the button again, stop recording
+            filename = self.filename
+            self.recFile.stop_recording()
+            # Show the user that the recording has ended.
+            self.recording_edit.setText(f"{tx('Recording_message_1')}'{filename}'{tx('Recording_message_2')}")
+            # Give the user the option to start a new recording
+            self.button_record.setText(tx("Start_voice_recording"))
+            self.button_state = 'record'
+            # Close the soundfile where the speech was recorded. Once it is closed, it is available for transcription
+            self.recFile.close()
+    
+    def open_file_dialog(self):
+        # Define where the program should look for audio files
+        filepath = QStandardPaths.writableLocation(QStandardPaths.StandardLocation.DocumentsLocation)
+        if not os.path.exists(filepath + tx("folder_name")):
+            os.mkdir(filepath + tx("folder_name"))
+        filepath = filepath + tx("folder_name")
+        os.chdir(filepath)
+
+        # Start 'choose a file' dialog
+        filenametuple = QFileDialog.getOpenFileName(
+            self,
+            tx("Choose_file"),
+           str(filepath),
+            "Audio (*.wav *.mp3 *.m4a *.flac)" # Select which file type to allow.
+        )
+        # Change directory.
+        os.chdir(filepath)
+
+        # Get the filename the user has choosen. Display it using setText.
+        filename = filenametuple[0]
+        if filename:
+            path = Path(filename)
+            self.filename_edit.setText(str(path.name))
+            self.filename = filename
+            self.filename_path = path
+
+
+    def choose_accuracy(self):
+        "This method allows the user to choose how precise and fast the transcription should be."
+        # Define options: Fast and imprecise, medium, slow and precise, very slow and very precise
+        items = [tx("Option_fast"), tx("Option_medium"), tx("Option_slow"), tx("Option_very_slow")]
+
+        # Create field to allow the user to choose
+        item, ok = QInputDialog.getItem(self,  tx("Transcription_setting"), tx("Text_transcription_setting"), items, 0, False)
+        
+        # Once the user gave their input, depending on user choice, set the accuracy variable and show the user what the current setting is.
+        if ok and item:
+            if item == tx("Option_fast"):
+                self.accuracy = 1
+                self.accuracy_edit.setText(tx("Setting_fast"))
+            if item == tx("Option_medium"):
+                self.accuracy = 2
+                self.accuracy_edit.setText(tx("Setting_medium"))
+            if item == tx("Option_slow"):
+                self.accuracy = 3
+                self.accuracy_edit.setText(tx("Setting_slow"))
+            if item == tx("Option_very_slow"):
+                self.accuracy = 4
+                self.accuracy_edit.setText(tx("Setting_very_slow"))
+            
+
+    def choose_translation(self):
+        "This method allows the user to choose if the text should be translated to English."
+        # Define options: Yes or No.
+        items = [tx("No"), tx("Yes")]
+
+        # Create field to allow user to choose
+        item, ok =  QInputDialog.getItem(self, tx("Translation_setting"), tx("Text_Translation_setting"), items, 0, False)
+
+        # Once the user gave their input, depending on user choice, set the translation-variable to 1 or 0 and show the user what the current setting is
+        if ok and item == tx("Yes"):
+            self.translation = 1
+            self.translation_edit.setText(tx("Yes"))
+        if ok and item == tx("No"):
+            self.translation = 0
+            self.translation_edit.setText(tx("No"))
+
+    def transcription_message(self):
+        "This method makes the message appear that the transcription started. It runs before the 'transcribe' function."
+        self.transcription_edit.setText(tx("Transcription_started"))
+
+
+    def transcribe(self):
+        " This function turns a m4a-file into mp3 if the supplied file is a m4a-file. It then transcribes the audio file to a txt-file."
+        # Get objects from previous user inputs:
+        try:
+            filename = self.filename_path.name
+        except AttributeError:
+                self.transcription_edit.setText(tx("No_file_selected"))
+                dlg = QMessageBox.warning(self,tx("No_file_warning"),tx("No_file_text"))
+                if dlg:
+                    return
+        filetype =self.filename_path.suffix
+        filename_stem = self.filename_path.stem
+        filepath = self.filename_path.parent
+        translation = self.translation
+        accuracy = self.accuracy
+
+        # Change directory to filepath again.
+        # This is required to make sure the user can change the directory in the openfilebutton and the program will still find the file he picked.
+        os.chdir(filepath)
+
+        # if the given file is of type m4a, it gets converted to mp3.
+        if filetype == '.m4a': # Import m4a file and export it as mp3
+            raw_audio = AudioSegment.from_file(f"{filename}", format='m4a')
+            raw_audio.export(f"{filename_stem}.mp3", format='mp3')
+            filename = f"{filename_stem}.mp3"
+        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        # Set up information needed to use Whisper
+        if translation == 1:
+            # For translation, use Whisper from Huggingface
+            # Set type of float used
+            torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+            # Set model id to be used.
+            if accuracy == 1:
+                model_id = "openai/whisper-tiny"
+            elif accuracy == 2:
+                model_id = "openai/whisper-small"
+            elif accuracy == 3:
+                model_id = "openai/whisper-medium"
+            elif accuracy == 4:
+                model_id = "openai/whisper-large-v3"
+            # Load model
+            model = AutoModelForSpeechSeq2Seq.from_pretrained(
+                model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+            )
+            model.to(device)
+
+            processor = AutoProcessor.from_pretrained(model_id)
+
+            pipe = pipeline(
+                "automatic-speech-recognition",
+                model=model,
+                tokenizer=processor.tokenizer,
+                feature_extractor=processor.feature_extractor,
+                max_new_tokens=128,
+                chunk_length_s=30,
+                batch_size=16,
+                return_timestamps=True,
+                torch_dtype=torch_dtype,
+                device=device,
+            )
+
+            filesize = Path(f'{filename}').stat().st_size
+            if filesize > 0:
+                # Start transcription and translation:
+                result = pipe(filename, generate_kwargs={"task": "translate"})
+            else: 
+                self.transcription_edit.setText(tx("No_sound_found"))
+                return
+            # Write the result to a .txt file:
+            with open(f'{filename_stem}.txt', 'w') as f:
+                try:
+                    f.write(result["text"])  # Can I show a message across several lines?
+                except UnicodeEncodeError:
+                    dlg = QMessageBox.warning(self,tx("No_sound_warning"),tx("No_sound_text"))
+                    if dlg:
+                        return
+        else:
+            # If no translation is requested, use fasterWhisper
+            if accuracy == 1:
+                model_size = "tiny"
+            elif accuracy == 2:
+                model_size = "small"
+            elif accuracy == 3:
+                model_size = "medium"
+            elif accuracy == 4:
+                model_size = "large-v3"
+
+            # Use float16 if the computer supports it
+            if torch.cuda.is_available():
+                model = WhisperModel(model_size, device=device, compute_type="float16")
+            else:
+                model = WhisperModel(model_size, device=device, compute_type="float32")
+
+
+            filesize = Path(f'{filename}').stat().st_size
+            if filesize > 0:
+                segments, info = model.transcribe(filename, beam_size=5) # What does a change in beam size do?
+                # Creates a new file where the transcription will be stored:
+                f = open(f'{filename_stem}.txt', 'w')
+                f.close()
+
+                #print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+                try:
+                    for segment in segments:
+                        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+                        with open(f'{filename_stem}.txt', 'a') as f:
+                            f.write(segment.text)  # Can I show a message across several lines?
+                except UnicodeEncodeError:
+                    dlg = QMessageBox.warning(self,tx("No_sound_warning"),tx("No_sound_text"))
+                    if dlg:
+                        return
+
+            else: 
+                self.transcription_edit.setText(tx("No_sound_found"))
+                return
+
+        self.transcription_edit.setText(f"{tx('Transcription_message_1')}'{filename_stem}.txt' {tx('Transcription_message_2')}")
+
+class Recorder(object):
+    # A recorder class for recording audio to a WAV file. Records in mono by default.
+
+    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024):
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+
+    def open(self, fname, mode='wb'):
+        return RecordingFile(fname, mode, self.channels, self.rate,
+                            self.frames_per_buffer)
+
+class RecordingFile(object):
+    def __init__(self, fname, mode, channels, 
+                rate, frames_per_buffer):
+        self.fname = fname
+        self.mode = mode
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+        self._pa = pyaudio.PyAudio()
+        self.wavefile = self._prepare_file(self.fname, self.mode)
+        self._stream = None
+
+    def start_recording(self):
+        # Use a stream with a callback in non-blocking mode
+        self._stream = self._pa.open(format=pyaudio.paInt16,
+                                            channels=self.channels,
+                                            rate=self.rate,
+                                            input=True,
+                                            frames_per_buffer=self.frames_per_buffer,
+                                            stream_callback=self.get_callback())
+        self._stream.start_stream()
+        return self
+
+    def stop_recording(self):
+        self._stream.stop_stream()
+        return self
+
+    def get_callback(self):
+        def callback(in_data, frame_count, time_info, status):
+            self.wavefile.writeframes(in_data)
+            return in_data, pyaudio.paContinue
+        return callback
+
+
+    def close(self):
+        self._stream.close()
+        self._pa.terminate()
+        self.wavefile.close()
+
+    def _prepare_file(self, fname, mode='wb'):
+        wavefile = wave.open(fname, mode)
+        wavefile.setnchannels(self.channels)
+        wavefile.setsampwidth(self._pa.get_sample_size(pyaudio.paInt16))
+        wavefile.setframerate(self.rate)
+        return wavefile
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # Create and display main window
+    window = MainWindow()
+
+    # Start the event loop
+    sys.exit(app.exec())
--- lang.py
+++ lang.py
@@ -4,20 +4,115 @@ language switch module.
 Provides the TEXT dictionary containing the translation
 for all text elements in the GUI.
 """
-import os
-import warnings
 
+import warnings
+import locale
 
 # get language setting from environment variable
-LANG = os.getenv("LANG").upper() or "EN"
-LANG = "EN" if "EN" in LANG else "DE"
+LANG = "DE" if "DE" in locale.getlocale()[0].upper() else "EN"
 
 TEXT_EN = {
-    "window_title": "Transcription software Whisper:",
+    "Window_title": "Transcription software Whisper:",
+    "Start_voice_recording": "Start voice recording",
+    "Voice_recording_not_yet_started": "Voice recording has not yet been started.",
+    "Select_a_file": "Select a file",
+    "Select_speed_and_accuracy": "Select speed and accuracy",
+    "Default_precision": "Medium speed and precision",
+    "Translation_selection":'Select: Should the text be translated into English?',
+    "Default_translation": "No",
+    "Transcription_not_started_yet":"Transcription has not yet been started.",
+    "Start_transcription":'Start transcription',
+    "Close_program": "Close program",
+    "Whisper_words_to_text": "Whisper converts spoken words into text.",
+    "Record_something":'Record something you say:',
+    "Choose_file":'Or select a file to be transcribed:',
+    'Voice_recording_started': "Voice recording has started.",
+    'folder_name': '/Sound recordings',
+    'file_name':"voice_recording",
+    "Microphone_not_recognized": "Microphone not recognized. Please try a different microphone or use different software for voice recording.",
+    "Stop_recording": "Stop voice recording",
+    "Recording_message_1":"Voice recording has ended.\n The recording has been saved under the name ",
+    "Recording_message_2": " in the 'Sound recordings' folder. \n You can convert it directly to text by clicking on 'Start transcription', start a new recording or select another file to transcribe.",
+    "Choose_file_button":  "Choose a file",
+    "Choosen_file": "Ausgewählte Datei:",
+    "Option_fast":"Fastest, low precision",
+    "Option_medium":"Medium speed and precision",
+    "Option_slow":"Slow, high precision",
+    "Option_very_slow":"Slowest, highest precision",
+    "Setting_fast":"Fast, low precision",
+    "Setting_medium":"Medium speed and precision",
+    "Setting_slow":"Slow, high precision",
+    "Setting_very_slow":"Very slow, very high precision",
+    "Transcription_setting":"Choice of precision and speed",
+    "Text_transcription_setting":"How precise should the translation be? More precise translations require more time.",
+    "No": "No",
+    "Yes": "Yes",
+    "Text_Translation_setting":  "Should the text be translated?",
+    "Translation_setting":"Text translation:",
+    "Transcription_started":"Transcription has started. Please wait...",
+    "No_file_selected":"Transcription cannot start yet. Select an audio file or start a new voice recording.",
+    "No_file_warning": "You have not selected an audio file yet",
+    "No_file_text":"You must select an audio file or make a new voice recording so that Whisper can transcribe it.",
+    "No_sound_found": "This file contains no sound. \n The microphone may have been broken. \n Try another one or make a new one.",
+    "No_sound_warning": "No speech detected",
+    "No_sound_text": "Whisper did not recognize any words. Perhaps the background noise in the recording was too strong, or you selected a recording in which nobody said anything.",
+    "Transcription_message_1": "Transcription has been completed. \n The file ",
+    "Transcription_message_2":".txt' contains the text and can be opened with any text editor (e.g. an Office program). It can be found in the same folder as the audio file."
+
+
+
+    
 }
 
 TEXT_DE = {
-    "window_title": "Transkriptionssoftware Whisper:",
+    "Window_title": "Transkriptionssoftware Whisper:",
+    "Start_voice_recording": "Sprachaufnahme starten",
+    "Voice_recording_not_yet_started":"Sprachaufnahme wurde noch nicht gestartet.",
+    "Select_a_file": "Datei auswählen",
+    "Select_speed_and_accuracy": "Geschwindigkeit und Genauigkeit auswählen",
+    "Default_precision": "Mittlere Geschwindigkeit und Präzision",
+    "Translation_selection":'Auswählen: Soll der Text auf Englisch übersetzt werden?',
+    "Default_translation": "Nein",
+    "Transcription_not_started_yet":"Transkription wurde noch nicht gestartet.",
+    "Start_transcription":'Transkribieren starten',
+    "Close_program": "Programm schließen",
+    "Whisper_words_to_text":'Whisper wandelt gesprochene Worte in Text um.',
+    "Record_something":'Nimm etwas auf, was du sagst:',
+    "Choose_file":'Oder wähle eine Datei aus, die transkribiert werden soll:',
+    'Voice_recording_started': "Sprachaufnahme wurde gestartet.",
+    'folder_name':'/Soundaufnahmen',
+    'file_name':"Sprachaufnahme",
+    "Microphone_not_recognized": "Mikrofon nicht erkannt. Bitte versuche ein anderes Mikrofon oder benutze eine andere Software für die Sprachaufnahme.",
+    "Stop_recording": "Sprachaufnahme beenden",
+    "Recording_message_1":"Sprachaufnahme wurde beendet.\n Die Aufnahme wurde unter dem Namen ",
+    "Recording_message_2": " im Ordner 'Soundaufnahmen' gespeichert. \n Du kannst sie mit einem Klick auf 'Transkribieren starten' direkt in Text umwandeln, eine neue Aufnahme starten oder eine andere Datei zum transkribieren auswählen.",
+    "Choose_file_button":  "Wähle eine Datei aus",
+    "Choosen_file": "Ausgewählte Datei:",
+    "Option_fast":"Am schnellsten, geringe Präzision",
+    "Option_medium": "Mittlere Geschwindigkeit und Präzision",
+    "Option_slow":"Langsam, hohe Präzision",
+    "Option_very_slow":  "Am langsamsten, höchste Präzision",
+    "Setting_fast": "Einstellung: Schnell, wenig präzise",
+    "Setting_medium":"Einstellung: Mittlere Geschwindigkeit und Präzision",
+    "Setting_slow":"Einstellung: Langsam, hohe Präzision",
+    "Setting_very_slow":"Einstellung: Sehr langsam, sehr hohe Präzision",
+    "Transcription_setting":"Wahl der Präzision und Geschwindigkeit",
+    "Text_transcription_setting":"Wie präzise soll die Übersetzung sein? Genauere Übersetzungen benötigen mehr Zeit.",
+    "No": "Nein",
+    "Yes": "Ja",
+    "Text_Translation_setting":   "Soll der Text übersetzt werden?",
+    "Translation_setting": "Textübersetzung:",
+    "Transcription_started":"Transkription wurde gestartet. Bitte warten...",
+    "No_file_selected":"Transkription kann noch nicht starten. Wähle eine Audiodatei aus oder nimm einige gesprochene Worte auf.",
+    "No_file_warning": "Du hast noch keine Audiodatei ausgewählt",
+    "No_file_text":"Du musst eine Audiodatei auswählen oder eine neue Sprachaufnahme machen, damit Whisper sie transkribieren kann.",
+    "No_sound_found": "Diese Datei enthält keinen Ton. \n Möglicherweise war das Mikrofon kaputt. \n Versuche es mit einer anderen oder fertige eine neue an.",
+    "No_sound_warning": "Keine Sprache erkannt",
+    "No_sound_text": "Whisper hat keine Worte erkannt. Vielleicht war das Hintergrundrauschen in der Aufnahme zu stark, oder du hast eine Aufnahme ausgewählt, in der niemand etwas gesagt hat.",
+    "Transcription_message_1": "Transkription wurde abgeschlossen. \n Die Datei ",
+    "Transcription_message_2":".txt' enthält den Text und kann mit einem beliebigen Texteditor (bspw. einem Office-Programm) geöffnet werden. Sie ist im selben Ordner zu finden wie die Audiodatei."
+
+
 }
 
 def get_text(key: str):
